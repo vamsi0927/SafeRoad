34f8b2b20dd6f26c8bb13d459e041b168ee0edd0
vamsi0927 <attalurivamsi0905@gmail.com>
2026-02-17 19:21:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,3 @@ def receive_sensor_data():
         return jsonify({"error": str(e)}), 400
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
